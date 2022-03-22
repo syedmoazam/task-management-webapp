ef9bc78d126d0c63478b4b34ab65893ba6a339eb
Syed Moazam Hussain <syedmoazam01@gmail.com>
2022-03-22 17:58:55 +0500

    setup: initialize the project

---
+++ src/config/Firebase/index.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, set, ref, child, get, push, remove } from 'firebase/database';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+const firebaseConfig = {
+    apiKey: "AIzaSyDOwGJ0AP3FGWbCInoMXhBbimfEARvSJNU",
+    authDomain: "redux-to-do-app-213de.firebaseapp.com",
+    projectId: "redux-to-do-app-213de",
+    storageBucket: "redux-to-do-app-213de.appspot.com",
+    messagingSenderId: "2770976191",
+    appId: "1:2770976191:web:0f2d564da711c51b75f50a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getDatabase();
+
+export {
+    db,
+    auth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    set,
+    ref,
+    child,
+    get,
+    push,
+    onAuthStateChanged,
+    signOut,
+    remove
+}
